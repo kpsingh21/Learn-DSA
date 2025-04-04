10255577207ae00f9e87b23537ee5b5b830caf46
Krishnapal singh Chouhan <104709843+kpsingh21@users.noreply.github.com>
2025-04-04 06:53:01 +0000

    Find the repeating and missing numbers

--- 05_Arrays_problems/Find_repeating_and_missing_no.js
+++ 05_Arrays_problems/Find_repeating_and_missing_no.js
@@ -26,3 +26,39 @@ function findMissingRepeatingNumbers(arr) {
 console.log(findMissingRepeatingNumbers([3, 1, 2, 5, 4, 6, 7, 5]))
 
 
+//Optimal Approach 1 (Using Maths): 
+
+
+function findMissingRepeatingNumbers2(arr) {
+
+
+    let n = arr.length;
+    // Find Sn and S2n:
+    const SN = (n * (n + 1)) / 2;
+    const S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+    let S = 0, S2 = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        S += arr[i];
+        S2 += arr[i] * arr[i];
+    }
+
+    //S-Sn = X-Y:
+    const val1 = S - SN;
+
+    // S2-S2n = X^2-Y^2:
+    let val2 = S2 - S2N;
+
+    //Find X+Y = (X^2-Y^2)/(X-Y):
+    val2 = val2 / val1;
+
+    //Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
+    // Here, X-Y = val1 and X+Y = val2:
+    const x = (val1 + val2) / 2;
+    const y = x - val1;
+
+    return [x, y];
+
+}
+
+console.log(findMissingRepeatingNumbers2([3, 1, 2, 5, 4, 6, 7, 5]))
